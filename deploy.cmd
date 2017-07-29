687704f37180d457ff8de23219f39991a9adc11c
Benjamin Kane <bbk1524@gmail.com>
2017-07-29 06:21:10 +0000

    Don't do eveything as root

--- deploy.cmd
+++ deploy.cmd
@@ -1,1 +1,3 @@
+ REM This is so I can push things from Windows to my backup box. It's not so important anymore
+ REM because I'm now working straight from the backup box itself
  rsync --recursive --stats ./ vagrant@10.0.0.10:/home/vagrant/Git/riced/
